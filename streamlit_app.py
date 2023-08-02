152f9d734b086561ffc33ccfdbe46771dea1e0a5
shreeyashah09 <shreeya.shah16@gmail.com>
2023-08-02 15:29:10 -0400

    adding streamlit_app

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+
+model_path = 'car_object_detection\yolov8n.pt'  # Replace with the path to your YOLOv8 model weights
+yolo = YOLO(model_path)
+
+# Setting page layout
+st.set_page_config(
+    page_title="Object Detection",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Creating sidebar
+with st.sidebar:
+    st.header("Image Config")
+    source_img = st.file_uploader("Upload an image...", type=("jpg", "jpeg", "png", "bmp", "webp"))
+    confidence = float(st.slider("Select Model Confidence", 25, 100, 40)) / 100
+
+
+if source_img is not None:
+    st.title("Object Detection")
+    # Load the uploaded image
+    img = Image.open(source_img)
+
+    # Run object detection on the image
+    results = yolo(source_img, conf=confidence)
+
+    # Display the original image with bounding boxes around detected objects
+    st.image(results.render()[0])
