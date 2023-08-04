d5cdfa02d44f822ba2640c19d013a8ab39b3a124
shreeyashah09 <shreeya.shah16@gmail.com>
2023-08-04 00:35:00 -0400

    adding streamlit app and adjustment in requirement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,135 +1,56 @@
 import streamlit as st
-from ultralytics import YOLO
+import cv2
+import numpy as np
 from PIL import Image
-from pathlib import Path
-import PIL
+from ultralytics import YOLO
+import torch
 
-from django.conf import settings
-import helper
+# Load the YOLOv8 model with the best weights
+def load_yolov8_model():
+    model = YOLO('best.pt')
+    return model 
 
-#model_path = 'best.pt'  # Replace with the path to your YOLOv8 model weights 
-#yolo = YOLO(model_path)
+def detect_objects_yolov8(image, model):
+    # YOLOv8 object detection code here
+    results = model.predict(image)
+    return results
 
-# C:\Users\ks010\Desktop\GBC\13. Deep Learning II\Project - Car Object Detection\car_object_detection\best.pt
+# Streamlit app
+def main():
+    st.title("Car Object Detection App")
+    st.write("Upload an image and let YOLOv8 detect objects in it!")
 
-# Setting page layout
-st.set_page_config(
-    page_title=" Car Object Detection using YOLOv8",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+    # File upload
+    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Main page heading
-st.title("Object Detection using YOLOv8")
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-# Sidebar
-st.sidebar.header("ML Model Config")
+        # Convert the image to OpenCV format
+        image_cv = np.array(image)
 
-# Model Options
-model_type = st.sidebar.radio(
-    "Select Task", ['Detection', 'Segmentation'])
+        # Load YOLOv8 model (call the loading function you have)
+        model = load_yolov8_model()
 
-confidence = float(st.sidebar.slider(
-    "Select Model Confidence", 25, 100, 40)) / 100
+        # Detect objects using YOLOv8
+        results = detect_objects_yolov8(image_cv, model)
 
-# Creating sidebar
-# with st.sidebar:
-#   st.header("Image Config")
-#   model_type = st.file_uploader("Upload an image...", type=("jpg", "jpeg", "png", "bmp", "webp"))
-#   confidence = float(st.slider("Select Model Confidence", 25, 100, 40)) / 100
+        # Draw bounding boxes on the image
+        detected_image = image_cv.copy()
+        for result in results.pred:
+            for box in result[:, :4]:
+                x_min, y_min, x_max, y_max = box
+                color = (0, 255, 0)  # Green color for bounding boxes
+                thickness = 2
+                cv2.rectangle(detected_image, (x_min, y_min), (x_max, y_max), color, thickness)
 
-# Selecting Detection Or Segmentation
-if model_type == 'Detection':
-    model_path = Path(settings.DETECTION_MODEL)
-elif model_type == 'Segmentation':
-    model_path = Path(settings.SEGMENTATION_MODEL)
+        # Convert back to PIL format for displaying
+        detected_image_pil = Image.fromarray(detected_image)
 
-# Load Pre-trained ML Model
-try:
-    model = helper.load_model(model_path)
-except Exception as ex:
-    st.error(f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
+        # Display the detected image
+        st.image(detected_image, caption="Detected Objects", use_column_width=True)
 
-st.sidebar.header("Image/Video Config")
-source_radio = st.sidebar.radio(
-    "Select Source", settings.SOURCES_LIST)
-
-source_img = None
-# If image is selected
-if source_radio == settings.IMAGE:
-    source_img = st.sidebar.file_uploader(
-        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        try:
-            if source_img is None:
-                default_image_path = str(settings.DEFAULT_IMAGE)
-                default_image = PIL.Image.open(default_image_path)
-                st.image(default_image_path, caption="Default Image",
-                         use_column_width=True)
-            else:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Uploaded Image",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Error occurred while opening the image.")
-            st.error(ex)
-
-    with col2:
-        if source_img is None:
-            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-            default_detected_image = PIL.Image.open(
-                default_detected_image_path)
-            st.image(default_detected_image_path, caption='Detected Image',
-                     use_column_width=True)
-        else:
-            if st.sidebar.button('Detect Objects'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
-
-elif source_radio == settings.VIDEO:
-    helper.play_stored_video(confidence, model)
-
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
-
-elif source_radio == settings.RTSP:
-    helper.play_rtsp_stream(confidence, model)
-
-elif source_radio == settings.YOUTUBE:
-    helper.play_youtube_video(confidence, model)
-
-else:
-    st.error("Please select a valid source type!")
-
-
-
-
-'''
-if source_img is not None:
-    st.title(" Car Object Detection")
-    # Load the uploaded image
-    img = Image.open(source_img)
-
-    # Run object detection on the image
-    results = yolo(source_img, conf=confidence)
-
-    # Display the original image with bounding boxes around detected objects
-    st.image(results.render()[0])
-'''
+if __name__ == "__main__":
+    main()
